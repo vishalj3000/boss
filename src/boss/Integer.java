096f3e3694dbf5b82c7bdde2fa4be5f3851754fe
Vishal <Vishal@LAPTOP-DTUF60DF>
2022-08-16 22:36:38 +0530

    i am sharing boss project details with you

---
+++ src/boss/Integer.java
@@ -0,0 +1,17 @@
+package boss;
+
+public class Integer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 20;
+		
+		int add = a+b;
+		
+		System.out.println("addition of a and b is "  +add );
+
+	}
+
+}
